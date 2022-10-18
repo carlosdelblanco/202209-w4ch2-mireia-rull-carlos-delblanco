9db344ef84587221fb1038a3b39250724ca802d7
Mireia Rull <mireia.rullmasdeu@gmail.com>
2022-10-18 20:50:50 +0200

    Add hangman component

--- src/components/Letters/Letters.js
+++ src/components/Letters/Letters.js
@@ -1,13 +1,18 @@
 import "./Letters.css";
 
-const Letters = () => {
+const Letters = ({ onLetterClick }) => {
   const alphabet = "abcdefghijklmnñopqrstuvwxyz";
+
+  const getLetterClick = (event) => {
+    event.preventDefault();
+    onLetterClick(event.target.textContent);
+  };
 
   return (
     <ul className="letters">
       {alphabet.split("").map((letter, key) => (
         <li key={key} className="letter">
-          <a href={letter} action="">
+          <a href={letter} action={getLetterClick}>
             {letter}
           </a>
         </li>
